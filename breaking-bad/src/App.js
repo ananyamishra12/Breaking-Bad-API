cbd5edc6421640867e9005ec79acdbde6d6a89dd
Ananya Mishra <66728375+ananyamishra12@users.noreply.github.com>
2022-06-01 18:56:16 +0530

    useEffect

--- breaking-bad/src/App.js
+++ breaking-bad/src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import Header from './components/Header';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
-function App() {
+const App= ()=> {
+  const [items, setItems]=useState([])
+  const [isLoading, setIsLoading]=useState(true)
+
+  useEffect(() =>{
+    const fetchItems= async() => {
+      const result= await axios(`https://www.breakingbadapi.com/api/characters`)
+      
+      console.log(result.data);
+    }
+
+    fetchItems();
+  }, [])
   return (
-    <div className="App">
-      <h1>check</h1>
+    <div className="container">
+      <Header />
     </div>
   );
 }
